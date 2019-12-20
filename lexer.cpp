04954d450b88c53a6cffa5298c5cfd42d87195e5
allen.bronshtien <allen.bronshtein@gmail.com>
2019-12-20 04:03:54 +0200

    started lexer main

--- lexer.cpp
+++ lexer.cpp
@@ -1,19 +1,18 @@
 #ifndef LEXER_CPP
 #define LEXER_CPP
+
 #include "lexer.h"
-list<list<string>> Lexer::lex(FILE *fp)
-{
+
+list <list<string>> Lexer::lex(FILE *fp) {
     std::string line;
     char buffer[100];
-    list<list<string>> lexer_list;
-    while (!feof(fp))
-    {
-        list<string> lexer_list_node;
+    list <list<string>> lexer_list;
+    while (!feof(fp)) {
+        list <string> lexer_list_node;
         fgets(buffer, 100, fp);
         line = buffer;
         //line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
-        if (line.find("openDataServer") != string::npos)
-        {
+        if (line.find("openDataServer") != string::npos) {
             std::string command, argument;
             command = line.substr(0, 14);
             int start_pos_arg = line.find("(") + 1, end_pos_arg = line.find(")") - 1;
@@ -21,8 +20,7 @@ list<list<string>> Lexer::lex(FILE *fp)
             lexer_list_node.push_front(argument);
             lexer_list_node.push_front(command);
             lexer_list.push_back(lexer_list_node);
-            for (auto i : lexer_list_node)
-            {
+            for (auto i : lexer_list_node) {
                 cout << i << endl;
             }
         }
@@ -55,5 +53,7 @@ list<list<string>> Lexer::lex(FILE *fp)
 
         // }
     }
+    return lexer_list;
 }
+
 #endif
